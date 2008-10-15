782ff4c0e93a17ba033876fef2cfa85be472d5db
Rich Dougherty <rich@rd.gen.nz>
2008-10-15 21:50:48 +1300

    Rough prototype of LLVM codegen from Llack IR.

---
+++ cpp/include/sample.h
@@ -0,0 +1,193 @@
+/*
+ * File: sample.h
+ *
+ *      This is a sample header file that is global to the entire project.
+ *      It is located here so that everyone will find it.
+ */
+//extern int compute_sample (int a);
+
+#include "llvm/DerivedTypes.h"
+#include "llvm/GlobalValue.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/Support/IRBuilder.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include <cstdio>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace llvm;
+
+typedef char* Stack;
+
+struct VMState {
+  Stack data;
+  Stack retain;
+  Stack cont;
+  Stack dataBottom;
+  Stack retainBottom;
+  Stack contBottom;
+};
+
+extern "C" VMState* newVMState();
+extern "C" void freeVMState(VMState* vmState);
+extern "C" void dumpVMState(VMState* vmState);
+
+class VMCodeGenInterface;
+class Word;
+
+class LlackModule {
+ public:
+  std::vector<Word*> words;
+};
+
+class LlackInstruction {
+ public:
+  virtual ~LlackInstruction();
+  virtual void codeGen(VMCodeGenInterface* cgi) = 0;
+};
+
+class Word {
+ public:
+  Word();
+  std::string name;
+  std::vector<LlackInstruction*> instructions;
+  static Word* Create(const std::string& name, LlackModule* mod);
+};
+
+class VMCodeGenInterface {
+ public:
+  // XXX: Needs anchor?
+  virtual ~VMCodeGenInterface();
+  virtual const TargetData* getTargetData() = 0;
+  virtual Type* getContType() = 0;
+  virtual Value* getWordCont(Word* word) = 0;
+  virtual void pushData(Value *v) = 0;
+  virtual Value* popData(const Type *t) = 0;
+  virtual void pushRetain(Value *v) = 0;
+  virtual Value* popRetain(Type *t) = 0;
+  virtual void pushCont(Value *v) = 0;
+  virtual Value* popCont() = 0;
+  virtual Instruction* addInstruction(Instruction* inst) = 0;
+};
+
+class SimpleVMCodeGenInterface : public VMCodeGenInterface {
+ private:
+  Module* mod;
+  const TargetData* td;
+  Value* vmStatePtr;
+  IRBuilder* builder;
+ public:
+ SimpleVMCodeGenInterface(Module* mod_, const TargetData* td_, Value* vmStatePtr_, IRBuilder* builder_):
+  mod(mod_), td(td_), vmStatePtr(vmStatePtr_), builder(builder_)
+  {}
+  virtual ~SimpleVMCodeGenInterface();
+  virtual const TargetData* getTargetData();
+  virtual Type* getContType();
+  virtual Value* getWordCont(Word* word);
+  virtual void pushData(Value *v);
+  virtual Value* popData(const Type *t);
+  virtual void pushRetain(Value *v);
+  virtual Value* popRetain(Type *t);
+  virtual void pushCont(Value *v);
+  virtual Value* popCont();
+  virtual Instruction* addInstruction(Instruction* inst);
+ private:
+  Type* getStackType();
+  Type* getVMStateType();
+  Value* getDataStack();
+  Value* getRetainStack();
+  Value* getContStack();
+  void push(Value* stack, Value* v);
+  Value* pop(Value* stack, const Type* t);
+};
+
+// XXX: Need sep instruction for Words until we implement
+// a LlackValue type.
+class PushWordLlackInst : public LlackInstruction {
+ private:
+  Word* word;
+ public:
+ PushWordLlackInst(Word* word_)
+   : word(word_)
+  {}
+  virtual ~PushWordLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class PushLlackInst : public LlackInstruction {
+ private:
+  Value* v;
+ public:
+ PushLlackInst(Value* v_)
+   : v(v_)
+  {}
+  virtual ~PushLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class ToContLlackInst : public LlackInstruction {
+ public:
+  ToContLlackInst() {}
+  virtual ~ToContLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class FromContLlackInst : public LlackInstruction {
+ public:
+  FromContLlackInst() {}
+  virtual ~FromContLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class SubLlackInst : public LlackInstruction {
+ private:
+  const Type* t;
+ public:
+ SubLlackInst(const Type* t_) : t(t_) {}
+  virtual ~SubLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class SelectLlackInst : public LlackInstruction {
+ private:
+  const Type* t;
+ public:
+ SelectLlackInst(const Type* t_) : t(t_) {}
+  virtual ~SelectLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class MulLlackInst : public LlackInstruction {
+ private:
+  const Type* t;
+ public:
+ MulLlackInst(const Type* t_) : t(t_) {}
+  virtual ~MulLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class ICmpLlackInst : public LlackInstruction {
+ private:
+  ICmpInst::Predicate p;
+  const Type* t;
+ public:
+ ICmpLlackInst(ICmpInst::Predicate p_, const Type* t_) : p(p_), t(t_) {}
+  virtual ~ICmpLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
+class ShuffleLlackInst : public LlackInstruction {
+ private:
+  std::vector<Type*> consumption;
+  std::vector<int> production;
+ public:
+ ShuffleLlackInst(std::vector<Type*> consumption_, std::vector<int> production_)
+   : consumption(consumption_), production(production_)
+  {}
+  virtual ~ShuffleLlackInst();
+  virtual void codeGen(VMCodeGenInterface* cgi);
+};
+
---
+++ cpp/lib/sample/sample.cpp
@@ -0,0 +1,266 @@
+/*
+ * File: sample.c
+ *
+ * Description:
+ *  This is a sample source file for a library.  It helps to demonstrate
+ *  how to setup a project that uses the LLVM build system, header files,
+ *  and libraries.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* LLVM Header File
+#include "llvm/Support/DataTypes.h"
+*/
+
+/* Header file global to this project */
+#include "sample.h"
+
+/*int
+compute_sample (int a)
+{
+  return a;
+  }*/
+
+
+//#include "llvm/BasicBlock.h"
+#include "llvm/DerivedTypes.h"
+#include "llvm/GlobalValue.h"
+#include "llvm/Module.h"
+#include "llvm/Analysis/Verifier.h"
+#include "llvm/Support/IRBuilder.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Target/TargetData.h"
+
+//#include "sample.h"
+
+using namespace llvm;
+
+/*class BlockBuilder {
+ private:
+  IRBuilder* builder;
+  Value* vmstatePtr;
+ public:
+  BlockBuilder(BasicBlock *bb, Value* _vmstatePtr)
+    : vmstatePtr(_vmstatePtr)
+  {
+  }
+  
+  //Value *insertInstruction(Instruction *inst) {
+  //  return builder->insert(inst);
+  //}
+  Value *insertPopData(IRBuilder* builder, Type *ty) {
+    Value *dataPtr = builder->CreateGEP(vmstatePtr, 0, "dataPtr");
+    Value *dataTop = builder->CreateLoad(dataPtr, "dataTop");
+    Value *elementSize = ConstantInt::get(Type::Int32Ty, 8); // XXX: calculate from ty
+    Value *newDataTop = builder->CreateSub(dataTop, elementSize);
+    Value *element = builder->CreateLoad(newDataTop, "element");
+    return element;
+  }
+  void insertPushData(Type *ty) {
+  }
+  BlockBuilder* insertConditional(Value* boolValue) {
+    return NULL;
+  }
+  };*/
+
+//SwapOp::~SwapOp() {}
+
+LlackInstruction::~LlackInstruction() {}
+
+Word::Word() {}
+
+Word* Word::Create(const std::string& name, LlackModule* mod) {
+  Word* word = new Word();
+  word->name = name;
+  mod->words.push_back(word);
+  return word;
+}
+
+VMCodeGenInterface::~VMCodeGenInterface() {}
+
+SimpleVMCodeGenInterface::~SimpleVMCodeGenInterface() {}
+const TargetData* SimpleVMCodeGenInterface::getTargetData() {
+  return td;
+}
+Type* SimpleVMCodeGenInterface::getContType() {
+  // XXX: could use actual function type; would make it
+  // easier to do tail call...
+  return PointerType::getUnqual(Type::Int8Ty);
+}
+Value* SimpleVMCodeGenInterface::getWordCont(Word* word) {
+  ConstantInt* intValue = ConstantInt::get(Type::Int32Ty, (int) word, false);
+  Value* ptrValue = builder->CreateIntToPtr(intValue, getContType());
+  return ptrValue;
+}
+void SimpleVMCodeGenInterface::pushData(Value *v) {
+  push(getDataStack(), v);
+}
+Value* SimpleVMCodeGenInterface::popData(const Type *t) {
+  return pop(getDataStack(), t);
+}
+void SimpleVMCodeGenInterface::pushRetain(Value *v) {
+  push(getRetainStack(), v);
+}
+Value* SimpleVMCodeGenInterface::popRetain(Type *t) {
+  return pop(getRetainStack(), t);
+}
+void SimpleVMCodeGenInterface::pushCont(Value *v) {
+  push(getContStack(), v);
+}
+Value* SimpleVMCodeGenInterface::popCont() {
+  return pop(getContStack(), getContType());
+}
+Instruction* SimpleVMCodeGenInterface::addInstruction(Instruction* inst) {
+  return builder->Insert(inst);
+}
+Type* SimpleVMCodeGenInterface::getStackType() {
+  return PointerType::getUnqual(Type::Int8Ty);
+}
+Type* SimpleVMCodeGenInterface::getVMStateType() {
+  return StructType::get(
+			 getStackType(), // data
+			 getStackType(), // retain
+			 getStackType(), // cont
+			 NULL
+			 );
+}
+Value* SimpleVMCodeGenInterface::getDataStack() {
+  return builder->CreateStructGEP(vmStatePtr, 0);
+}
+Value* SimpleVMCodeGenInterface::getRetainStack() {
+  return builder->CreateStructGEP(vmStatePtr, 1);
+}
+Value* SimpleVMCodeGenInterface::getContStack() {
+  return builder->CreateStructGEP(vmStatePtr, 2);
+}
+void SimpleVMCodeGenInterface::push(Value* stack, Value* v) {
+  const Type *t = v->getType();
+
+  Value *top = builder->CreateLoad(stack);
+  Value *castTop = builder->CreateBitCast(top, PointerType::getUnqual(t));
+  builder->CreateStore(v, castTop);
+
+  // XXX: align stack
+  int elementSize = td->getTypeStoreSize(t);
+  ConstantInt *topDelta = ConstantInt::get(Type::Int32Ty, elementSize, true);
+  Value *newTop = builder->CreateGEP(top, topDelta);
+  builder->CreateStore(newTop, stack);
+}
+Value* SimpleVMCodeGenInterface::pop(Value* stack, const Type* t) {
+  int elementSize = td->getTypeStoreSize(t);
+  ConstantInt *topDelta = ConstantInt::get(Type::Int32Ty, -1 * elementSize, true);
+  // move top down
+  Value *top = builder->CreateLoad(stack);
+  Value *newTop = builder->CreateGEP(top, topDelta);
+  builder->CreateStore(newTop, stack);
+  // load value
+  Value *castTop = builder->CreateBitCast(newTop, PointerType::getUnqual(t));
+  Value *topValue = builder->CreateLoad(castTop);
+  return topValue;
+}
+
+
+PushLlackInst::~PushLlackInst()  {}
+void PushLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  cgi->pushData(v);
+}
+
+PushWordLlackInst::~PushWordLlackInst()  {}
+void PushWordLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* v = cgi->getWordCont(word);
+  cgi->pushData(v);
+}
+
+ToContLlackInst::~ToContLlackInst()  {}
+void ToContLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* v = cgi->popData(cgi->getContType());
+  cgi->pushCont(v);
+}
+
+FromContLlackInst::~FromContLlackInst()  {}
+void FromContLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* v = cgi->popCont();
+  cgi->pushData(v);
+}
+
+SubLlackInst::~SubLlackInst()  {}
+void SubLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* b = cgi->popData(t);
+  Value* a = cgi->popData(t);
+  Value* c = cgi->addInstruction(BinaryOperator::createSub(a, b));
+  cgi->pushData(c);
+}
+
+MulLlackInst::~MulLlackInst()  {}
+void MulLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* b = cgi->popData(t);
+  Value* a = cgi->popData(t);
+  Value* c = cgi->addInstruction(BinaryOperator::createMul(a, b));
+  cgi->pushData(c);
+}
+
+SelectLlackInst::~SelectLlackInst()  {}
+void SelectLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* falseValue = cgi->popData(t);
+  Value* trueValue = cgi->popData(t);
+  Value* boolean = cgi->popData(Type::Int1Ty);
+  Value* result = cgi->addInstruction(SelectInst::Create(boolean, trueValue, falseValue));
+  cgi->pushData(result);
+}
+
+ICmpLlackInst::~ICmpLlackInst()  {}
+void ICmpLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  Value* rhs = cgi->popData(t);
+  Value* lhs = cgi->popData(t);
+  Value* result = cgi->addInstruction(new ICmpInst(p, lhs, rhs));
+  cgi->pushData(result);
+}
+
+ShuffleLlackInst::~ShuffleLlackInst()  {}
+void ShuffleLlackInst::codeGen(VMCodeGenInterface* cgi)  {
+  std::vector<Value*> values;
+  for (std::vector<Type*>::iterator iter = consumption.begin(); iter < consumption.end(); ++iter) {
+    Type* t = *iter;
+    Value* v = cgi->popData(t);
+    values.push_back(v);
+  }
+  std::reverse(values.begin(), values.end()); // XXX: Could avoid this for efficiency.
+  for (std::vector<int>::iterator iter = production.begin(); iter < production.end(); ++iter) {
+    int index = *iter;
+    Value* v = values[index];
+    cgi->pushData(v);
+  }
+}
+
+VMState* newVMState() {
+  VMState* vmState = new VMState();
+  vmState->dataBottom = vmState->data = new char[1024];
+  vmState->retainBottom = vmState->retain = new char[1024];
+  vmState->contBottom = vmState->cont = new char[1024];
+  return vmState;
+}
+void freeVMState(VMState* vmState) {
+  delete[] vmState->dataBottom;
+  delete[] vmState->retainBottom;
+  delete[] vmState->contBottom;
+}
+void dumpStack(Stack bottom, Stack top) {
+  printf("<");
+  for (Stack i = bottom; i < top; ++i) {
+    printf(" %02x", (unsigned char) *i);
+  }
+  printf(" >\n");
+}
+void dumpVMState(VMState* vmState) {
+  printf("----------\n");
+  printf("vmState: %p\n", (void*) vmState);
+  printf("vmState->data: %p\n", vmState->data);
+  dumpStack(vmState->dataBottom, vmState->data);
+  printf("vmState->retain: %p\n", vmState->retain);
+  dumpStack(vmState->retainBottom, vmState->retain);
+  printf("vmState->cont: %p\n", vmState->cont);
+  dumpStack(vmState->contBottom, vmState->cont);
+  printf("----------\n");
+}
